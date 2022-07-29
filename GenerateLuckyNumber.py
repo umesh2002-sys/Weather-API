d910dd057bb53c217c97380ed2ad376bd5d44f4c
Umesh <80591150+umesh2002-sys@users.noreply.github.com>
2022-07-29 12:07:02 +0545

    Delete GenerateLuckyNumber.py

--- GenerateLuckyNumber.py
+++
@@ -1,9 +0,0 @@
-import random
-from datetime import datetime
-
-lucky_Number = random.randint(1, 100)
-
-today = datetime.today()
-day = today.strftime("%B %d, %Y")
-
-print(f"Today is {day} and your lucky number is: {lucky_Number}")
